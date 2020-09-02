05873bed229999eb23a377540de1fcc7819e2620
xXangelXx <angeluchimag17@gmail.com>
2020-09-01 20:58:36 -0400

    start

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+
+    </div>
+  );
+}
+
+export default App;
+/*     <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,9 +24,4 @@ function App() {
         >
           Learn React
         </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+      </header>*/
---
+++ src/pages/index.jsx
@@ -0,0 +1,8 @@
+
+import React, {useState,useEffect} from 'react';
+
+const Index = () =>{
+
+}
+//https://pokeapi.co/api/v2/pokemon/{id or name}/
+export default Index
