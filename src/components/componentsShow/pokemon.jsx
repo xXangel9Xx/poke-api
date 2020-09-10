9725efed55ff8707be9cd6260a7a5fb03a2a9272
xXangelXx <angeluchimag17@gmail.com>
2020-09-09 20:12:45 -0400

    fixed componentsShow/pokemon types pokemon.jsx

--- src/components/componentsShow/pokemon.jsx
+++ src/components/componentsShow/pokemon.jsx
@@ -1,18 +1,32 @@
 import React, {useState,useEffect} from 'react';
 const Pokemon = (props) =>{
+    const [type1, setType1] = useState()
+    const [type2, setType2] = useState()
     useEffect(()=>{
+        try {
+            setType1(props.pokemonTypes[0])
+            if (props.pokemonTypes[1]) {
+                setType2(props.pokemonTypes[1])    
+            }
+        } catch (error) {
+            
+        }
     },[props.pokemonTypes])
+
+    useEffect(()=>{
+    },[type1])
+
 
     return(
         <div className="container-pokemon">
             <div className="date-pokemon">
                     <p>{props.pokemonName}</p>
                 <div className="container-information-basic">
-                    {props.pokemonTypes &&
+                    {type1 &&
                     <p>{props.pokemonTypes[0].type.name}</p>                    
                     }
-                    {props.pokemonTypes &&
-                        <p>{props.pokemonTypes[1].type.name}</p>                    
+                    {type2 && 
+                     <p>{props.pokemonTypes[1].type.name}</p>                    
                     }
                 </div>
             </div>
