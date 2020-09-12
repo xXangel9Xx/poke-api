20c3a0a15b650ab9d10b1737262d9113a0be630c
xXangelXx <angeluchimag17@gmail.com>
2020-09-12 02:20:34 -0400

    base states components/componentsShow/baseState.jsx ready

--- src/components/componentsShow/baseStates.jsx
+++ src/components/componentsShow/baseStates.jsx
@@ -1,9 +1,39 @@
 import React,{useState,useEffect} from 'react';
-const BaseState = () =>{
+import '../../assets/componentsShowAssets/baseState.css'
+const BaseState = (props) =>{
+    const [state,setState] = useState()
+    useEffect(()=>{
+       setState(props.stats)
+    },[props.stats])
 
+    useEffect(()=>{
+        console.log(state)
+    },[state])
     return (
-        <div>
+        <div className="container-base-state">
+            {state && 
+            <>
+            <div className="container-dates-base_state">
+                <p className="color-text-stat">{state[5].stat.name}:<span className="color-number-state">{state[5].base_stat}</span> <m className="measurer background-red"> <hr className="hr hr-red" style={{width: state[5].base_stat}}/></m></p>
+            </div>
 
+            <div className="container-dates-base_state">
+                <p className="color-text-stat">{state[4].stat.name}:<span className="color-number-state">{state[4].base_stat}</span><m className="measurer brackground-green"> <hr className="hr hr-green" style={{width: state[4].base_stat}}/></m></p>
+            </div>
+
+            <div className="container-dates-base_state">
+               <p className="color-text-stat">{state[3].stat.name}:<span className="color-number-state">{state[3].base_stat}</span><m className="measurer brackground-green"> <hr className="hr hr-green" style={{width: state[3].base_stat}}/></m></p>
+            </div>
+
+            <div className="container-dates-base_state">
+               <p className="color-text-stat">{state[2].stat.name}:<span className="color-number-state">{state[2].base_stat}</span><m className="measurer background-red"> <hr className="hr hr-red" style={{width: state[2].base_stat}}/></m></p>
+            </div>
+
+            <div className="container-dates-base_state">
+               <p className="color-text-stat">{state[1].stat.name}:<span className="color-number-state">{state[1].base_stat}</span><m className="measurer background-red"> <hr className="hr hr-red" style={{width: state[1].base_stat}}/></m></p>
+            </div>
+            </>
+            }
         </div>
     )
 }
--- src/components/componentsShow/board.jsx
+++ src/components/componentsShow/board.jsx
@@ -2,10 +2,17 @@ import React, {useState,useEffect} from 'react'
 import '../../assets/componentsShowAssets/board.css'
 import Pokemon from './pokemon'
 import About from './about'
+import BaseState from './baseStates'
 const Board = (props) => {
     useEffect(()=>{
+
     },[props.pokemon])
-    
+    /*
+                <About 
+                    pokemonHeight={props.pokemon.height}
+                    pokemonWeight={props.pokemon.weight}
+                    pokemonAbilities={props.pokemon.abilities}
+                />*/
     return(
         <div className="board-show-pokemon">
             <Pokemon 
@@ -19,12 +26,9 @@ const Board = (props) => {
                 <div className="details states"><p>Base states</p></div>
                 <div className="details details-moves"><p>moves</p></div>
             </div>
-
-                <About 
-                    pokemonHeight={props.pokemon.height}
-                    pokemonWeight={props.pokemon.weight}
-                    pokemonAbilities={props.pokemon.abilities}
-                />
+            <BaseState 
+                    stats={props.pokemon.stats}
+            />
             </div>
         </div>
     )
