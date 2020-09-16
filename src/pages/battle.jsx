8e7b4e4a355fb349c143b38e31ab4c1178824215
xXangelXx <angeluchimag17@gmail.com>
2020-09-15 20:42:19 -0400

    improved fetch battle

--- src/pages/battle.jsx
+++ src/pages/battle.jsx
@@ -4,13 +4,19 @@ const Battle = (props) =>{
     const [pokemonPlayer, setPokemonPlayer] = useState(props.match.params.id)
     const [pokemonMachine, setPokemonMachine] = useState( Math.floor(Math.random()*999)+1 )
     useEffect(()=>{
-         getPokemon(pokemonPlayer).then(val => setPokemonPlayer(val) )
-      
+        if (pokemonPlayer) {
+            getPokemon(pokemonPlayer).then(val => setPokemonPlayer(val) )            
+        } else {
+          let pokemon = Math.floor(Math.random()*999)+1
+          getPokemon(pokemon).then(val => setPokemonPlayer(val) )
+        }
     },[])
     useEffect(()=>{
         getPokemon(pokemonMachine).then(val => setPokemonMachine(val) )  
     },[])
-
+    useEffect(()=>{
+        console.log(pokemonPlayer)
+    },[pokemonPlayer])
     return (
         <div className="container-battle">
 
