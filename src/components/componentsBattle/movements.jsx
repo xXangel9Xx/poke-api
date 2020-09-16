57de2b3738c8660b276dfd4ae8c713232244369a
xXangelXx <angeluchimag17@gmail.com>
2020-09-16 03:25:22 -0400

    battle 300px ready

--- src/components/componentsBattle/movements.jsx
+++ src/components/componentsBattle/movements.jsx
@@ -1,4 +1,5 @@
 import React, {useState,useEffect} from 'react';
+import '../../assets/componentsBattleAssets/movements.css'
 const Movements = (props) =>{
     useEffect(()=>{
         console.log(props.movements)
@@ -8,7 +9,10 @@ const Movements = (props) =>{
         <>
         { props.movements &&
             <div className="container-movements">
-                
+                <div className="movement"><p>{props.movements[1].stat.name}</p></div>
+                <div className="movement"><p>{props.movements[2].stat.name}</p></div>
+                <div className="movement"><p>{props.movements[3].stat.name}</p></div>
+                <div className="movement"><p>{props.movements[4].stat.name}</p></div>
             </div>
         }
         </>
