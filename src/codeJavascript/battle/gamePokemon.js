a577e3c8f43930ee6ddf2ea7d6c34648a626ac05
xXangelXx <angeluchimag17@gmail.com>
2020-09-22 02:20:56 -0400

    battle pokemons

--- src/codeJavascript/battle/gamePokemon.js
+++ src/codeJavascript/battle/gamePokemon.js
@@ -3,24 +3,28 @@ import attackVsAttack from './attackVs/attackVsAttack'
 
 export default function gamePokemon(typeMovementPlayer,movementPlayer,hpPlayer,setHpPlayer,hpMachine,setHpMachine,movementsMachine){
      let random = Math.floor(Math.random()*4)+1 
-     let typeMovementMachine = movementsMachine[2]
+     let typeMovementMachine = movementsMachine[random]
+     console.log(typeMovementPlayer)
+     console.log(typeMovementMachine.stat.name)
      if (typeMovementPlayer == 'attack' && typeMovementMachine.stat.name == 'attack') {
           attackVsAttack(movementPlayer[1].base_stat,hpPlayer,setHpPlayer,typeMovementMachine.base_stat,hpMachine,setHpMachine)
      }else if (typeMovementPlayer == 'attack' && typeMovementMachine.stat.name == 'defense'){
           let result = attackVsDefense(movementPlayer[1].base_stat,typeMovementMachine.base_stat)
-          console.log(result)
-          
           if (hpMachine - result <= 0){
                setHpMachine(0) 
           }else if (result != false && hpMachine > 0){
                result = result - hpMachine
                setHpMachine(-result)                
           }
-
- 
-     }else if (typeMovementPlayer == 'attack' && typeMovementMachine.stat.name == 'special-attack'){
-
+     }else if (typeMovementPlayer == 'attack' && typeMovementMachine.stat.name == 'special-attack' || typeMovementPlayer == 'special-attack' ){
+          attackVsAttack(movementPlayer[3].base_stat,hpPlayer,setHpPlayer,typeMovementMachine.base_stat,hpMachine,setHpMachine)
      }else if (typeMovementPlayer == 'attack' && typeMovementMachine.stat.name == 'special-defense'){
-
+          let result = attackVsDefense(movementPlayer[1].base_stat,typeMovementMachine.base_stat)
+          if (hpMachine - result <= 0){
+               setHpMachine(0) 
+          }else if (result != false && hpMachine > 0){
+               result = result - hpMachine
+               setHpMachine(-result)                
+          }
      }
 }
