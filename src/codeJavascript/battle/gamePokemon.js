b5f55626fb6441a362dec75b552aac7dae0cd525
xXangelXx <angeluchimag17@gmail.com>
2020-09-22 00:39:31 -0400

    rename mkdir attackVs.. location:src/codeJavascript/battle/attackVs

--- src/codeJavascript/battle/gamePokemon.js
+++ src/codeJavascript/battle/gamePokemon.js
@@ -1,7 +1,8 @@
 export default function gamePokemon(typeMovementPlayer,movementPlayer,hpPlayer,setHpPlayer,hpMachine,setHpMachine,movementsMachine){
      let random = Math.floor(Math.random()*4)+1 
      let tipeMovementMachine = movementsMachine[random]
-     if (typeMovementPlayer == 'attack') {
-          alert(tipeMovementMachine.stat.name)
+     if (typeMovementPlayer == 'attack' && tipeMovementMachine.stat.name == 'attack') {
+     }else if (typeMovementPlayer == 'attack' && typeMovementPlayer.stat.name == 'defense'){
+
      }
 }
