61e5a6d0243439527e0a0347922d9e97e7c51442
xXangelXx <angeluchimag17@gmail.com>
2020-09-22 01:49:41 -0400

    arrack vs defense ready src/codeJavascript/battle/attackVsDefense

--- src/codeJavascript/battle/attackVs/attackVsDefense.js
+++ src/codeJavascript/battle/attackVs/attackVsDefense.js
@@ -1,3 +1,7 @@
-export default function attackVsDefense(){
-    
+export default function attackVsDefense(powerAttack,defense){
+        let total = powerAttack / 3 - defense / 3 
+        if (total > 0){ 
+            return parseInt(total)
+        }
+        return false
 }
--- src/codeJavascript/battle/gamePokemon.js
+++ src/codeJavascript/battle/gamePokemon.js
@@ -1,13 +1,23 @@
+import attackVsDefense from './attackVs/attackVsDefense'
 import attackVsAttack from './attackVs/attackVsAttack'
 
 export default function gamePokemon(typeMovementPlayer,movementPlayer,hpPlayer,setHpPlayer,hpMachine,setHpMachine,movementsMachine){
      let random = Math.floor(Math.random()*4)+1 
-     let typeMovementMachine = movementsMachine[1]
+     let typeMovementMachine = movementsMachine[2]
      if (typeMovementPlayer == 'attack' && typeMovementMachine.stat.name == 'attack') {
           attackVsAttack(movementPlayer[1].base_stat,hpPlayer,setHpPlayer,typeMovementMachine.base_stat,hpMachine,setHpMachine)
-
      }else if (typeMovementPlayer == 'attack' && typeMovementMachine.stat.name == 'defense'){
+          let result = attackVsDefense(movementPlayer[1].base_stat,typeMovementMachine.base_stat)
+          console.log(result)
+          
+          if (hpMachine - result <= 0){
+               setHpMachine(0) 
+          }else if (result != false && hpMachine > 0){
+               result = result - hpMachine
+               setHpMachine(-result)                
+          }
 
+ 
      }else if (typeMovementPlayer == 'attack' && typeMovementMachine.stat.name == 'special-attack'){
 
      }else if (typeMovementPlayer == 'attack' && typeMovementMachine.stat.name == 'special-defense'){
