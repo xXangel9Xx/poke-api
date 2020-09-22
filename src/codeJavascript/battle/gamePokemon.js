94f8900948ce482242a9638c5063b991a64d4b4e
xXangelXx <angeluchimag17@gmail.com>
2020-09-22 02:34:33 -0400

    ready attacks player

--- src/codeJavascript/battle/gamePokemon.js
+++ src/codeJavascript/battle/gamePokemon.js
@@ -8,13 +8,14 @@ export default function gamePokemon(typeMovementPlayer,movementPlayer,hpPlayer,s
      console.log(typeMovementMachine.stat.name)
      if (typeMovementPlayer == 'attack' && typeMovementMachine.stat.name == 'attack') {
           attackVsAttack(movementPlayer[1].base_stat,hpPlayer,setHpPlayer,typeMovementMachine.base_stat,hpMachine,setHpMachine)
-     }else if (typeMovementPlayer == 'attack' && typeMovementMachine.stat.name == 'defense'){
+     }else if (typeMovementPlayer == 'attack' && typeMovementMachine.stat.name == 'defense' || typeMovementMachine.stat.name == 'special-defense'){
           let result = attackVsDefense(movementPlayer[1].base_stat,typeMovementMachine.base_stat)
           conditionalChangeHp(result,hpMachine,setHpMachine)
-     }else if (typeMovementPlayer == 'attack' && typeMovementMachine.stat.name == 'special-attack' || typeMovementPlayer == 'special-attack' ){
+     }else if (typeMovementPlayer == 'special-attack' && typeMovementMachine.stat.name == 'special-attack'){
           attackVsAttack(movementPlayer[3].base_stat,hpPlayer,setHpPlayer,typeMovementMachine.base_stat,hpMachine,setHpMachine)
-     }else if (typeMovementPlayer == 'attack' && typeMovementMachine.stat.name == 'special-defense'){
-          let result = attackVsDefense(movementPlayer[1].base_stat,typeMovementMachine.base_stat)
+     }else if(typeMovementPlayer == 'special-attack' && typeMovementMachine.stat.name == 'defense' || typeMovementMachine.stat.name == 'special-defense'){
+          let result = attackVsDefense(movementPlayer[3].base_stat,typeMovementMachine.base_stat)
           conditionalChangeHp(result,hpMachine,setHpMachine)
      }
 }
+
